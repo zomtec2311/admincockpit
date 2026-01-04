1584f16262e80fbcef7c3d6d4d02e25affd614b5
zomtec2311 <wtoedt@gmail.com>
2026-01-04 15:48:52 +0100

    Add files via upload

---
+++ templates/index.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use OCP\Util;
+
+Util::addScript(OCA\AdminCockpit\AppInfo\Application::APP_ID, 'admincockpit-main');
+Util::addStyle(OCA\AdminCockpit\AppInfo\Application::APP_ID, 'admincockpit-main');
+?>
+
+<div id="admincockpit"></div>
